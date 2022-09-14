a99e4cca502dfedf7ae5046be015fb2a108cd6e6
Copy <phannguyendoremon@gmail.com>
2022-09-14 22:12:59 +0700

    update js port

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server);
-server.listen(3000,()=>{
-    console.log('kết nối tại cổng 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT,()=>{
+    console.log(`kết nối tại cổng ${PORT}`);
 })
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); 
 io.on('connection',(socket)=>{
